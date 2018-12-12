782ee089fd6a59fa87e0f1812c1e15cffaa4449c
Sho Hashimoto <sho.hsmt@gmail.com>
2018-12-13 00:29:29 +0900

    Add since/until tag to all redmine methods.

--- doc/yardoc/templates/rich_reference/fulldoc/html/setup.rb
+++ doc/yardoc/templates/rich_reference/fulldoc/html/setup.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 include Helpers::ModuleHelper
 
+require 'redmine/version'
+
 def init
   options.objects = objects = run_verifier(options.objects)
 
@@ -14,7 +16,31 @@ def init
   options.delete(:objects)
   options.delete(:files)
 
+  # TODO: Move to proper file like under layout.
+  require 'yaml'
+  yaml_path = File.expand_path("../../../../redmine_changes.yml", __dir__)
+  $redmine_changes = YAML.load(File.read(yaml_path))
+  redmine_ver =
+    Gem::Version.new(Redmine::VERSION.to_s.gsub(/\.(stable|devel)\z/, ''))
+
   objects.each do |object|
+    # TODO: Commonize with fulldoc like helper method.
+    $redmine_changes.select { |c|
+      c[:class_or_method] == object.to_s
+    }.reject { |c|
+      v = Gem::Version.new(c[:version])
+      (c[:event] == :added && v > redmine_ver) || (c[:event] == :removed && v < redmine_ver)
+    }.each do |change|
+      yard_event = {added: :since, removed: :deprecated}[change[:event]]
+      if tags = object.tags(yard_event).empty?
+        case yard_event
+        when :since
+          object.add_tag(YARD::Tags::Tag.new(:since, change[:version]))
+        when :deprecated
+          object.add_tag(YARD::Tags::Tag.new(:deprecated, "Removed at #{change[:version]}"))
+        end
+      end
+    end
     begin
       serialize(object)
     rescue => e
--- doc/yardoc/templates/rich_reference/method_details/setup.rb
+++ doc/yardoc/templates/rich_reference/method_details/setup.rb
@@ -1,5 +1,29 @@
 # frozen_string_literal: true
+
+require 'redmine/version'
+
 def init
+  # TODO: Commonize with fulldoc like helper method.
+  redmine_ver =
+    Gem::Version.new(Redmine::VERSION.to_s.gsub(/\.(stable|devel)\z/, ''))
+
+  $redmine_changes.select { |c|
+    c[:class_or_method] == object.to_s
+  }.reject { |c|
+    v = Gem::Version.new(c[:version])
+    (c[:event] == :added && v > redmine_ver) || (c[:event] == :removed && v < redmine_ver)
+  }.each do |change|
+    yard_event = {added: :since, removed: :deprecated}[change[:event]]
+    if tags = object.tags(yard_event).empty?
+      case yard_event
+      when :since
+        object.add_tag(YARD::Tags::Tag.new(:since, change[:version]))
+      when :deprecated
+        object.add_tag(YARD::Tags::Tag.new(:deprecated, "Removed at #{change[:version]}"))
+      end
+    end
+  end
+
   sections :header, [:method_signature, T('docstring'), :source]
 end
 
